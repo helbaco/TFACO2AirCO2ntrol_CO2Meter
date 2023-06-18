89d81445b8459b6c210820e76d4eef77f91e986d
helbaco <github.com@sombrasoft.de>
2023-06-18 17:49:46 +0200

    added exit after getting temp or co2 value

--- co2monitor.py
+++ co2monitor.py
@@ -2,6 +2,7 @@
 
 import sys, fcntl, time
 
+
 def decrypt(key,  data):
 	cstate = [0x48,  0x74,  0x65,  0x6D,  0x70,  0x39,  0x39,  0x65]
 	shuffle = [2, 4, 0, 7, 1, 6, 5, 3]
@@ -35,7 +36,7 @@ if __name__ == "__main__":
 	# Key retrieved from /dev/random, guaranteed to be random ;)
 	key = [0xc4, 0xc6, 0xc0, 0x92, 0x40, 0x23, 0xdc, 0x96]
 
-	if (len(sys.argv) != 2):
+	if (len(sys.argv) != 3):
 		print("Please specifiy the device, mostly it is /dev/hidraw0")
 		exit(1)
 	
@@ -57,22 +58,18 @@ if __name__ == "__main__":
 			decrypted = decrypt(key, data)
 
 		if decrypted[4] != 0x0d or (sum(decrypted[:3]) & 0xff) != decrypted[3]:
-			 print (hd(data), " => ", hd(decrypted),  "Checksum error")
+			print (hd(data), " => ", hd(decrypted),  "Checksum error")
 		else:
-				op = decrypted[0]
-				val = decrypted[1] << 8 | decrypted[2]
+			op = decrypted[0]
+			val = decrypted[1] << 8 | decrypted[2]
+				
+			values[op] = val
+
+			if 0x50 in values and sys.argv[2] == 'co2':
+				print ("%4i" % values[0x50]) 
+				exit(1)
+			if 0x42 in values and sys.argv[2] == 'temp':
+				print ("%2.2f" % (values[0x42]/16.0-273.15))
+				exit(1)
+			print
 			
-				values[op] = val
-			
-				# Output all data, mark just received value with asterisk
-			 	#print (", ".join( "%s%02X: %04X %5i" % ([" ", "*"][op==k], k, v, v) for (k, v) in sorted(values.items())), "  ",)
-			 	## From http://co2meters.com/Documentation/AppNotes/AN146-RAD-0401-serial-communication.pdf
-				if 0x50 in values:
-					print ("CO2: %4i" % values[0x50]) 
-				if 0x42 in values:
-					print ("T: %2.2f" % (values[0x42]/16.0-273.15))
-				if 0x44 in values:
-					print ("RH: %2.2f" % (values[0x44]/100.0))
-				if 0x41 in values:
-					print ("RH: %2.2f" % (values[0x41]/100.0))
-				print
